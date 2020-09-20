8ef67a2180a9ca6c824bda3a989fc1669cce9f5e
qianzhengkuan <40857634+qianzhengkuan@users.noreply.github.com>
2020-09-20 10:38:56 +0800

    Update App.vue
    解决小程序滚动条的问题

--- App.vue
+++ App.vue
@@ -26,4 +26,12 @@
 	@import "./common/free.css";
 	/* 全局样式 */
 	@import "./common/common.css";
+	/* 解决小程序和app滚动条的问题 */ /* #ifdef MP-WEIXIN || APP-PLUS */
+::-webkit-scrollbar {
+	display: none;
+} /* #endif */ /* 解决H5 的问题 */ /* #ifdef H5 */
+uni-scroll-view .uni-scroll-view::-webkit-scrollbar {
+	/* 隐藏滚动条，但依旧具备可以滚动的功能 */
+	display: none;
+} /* #endif */
 </style>
